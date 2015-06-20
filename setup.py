6b7f78406f3d6e64e174bccc41da9b4d26e034dd
Joel Bender <jjb5@cornell.edu>
2015-06-19 22:53:08 -0400

    add bacpypes requirement, bump the version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='modpypes',
-    version='0.1.0',
+    version='0.1.1',
     description="Python library for MODBUS based on BACpypes",
     long_description=readme + '\n\n' + history,
     author="Joel Bender",
