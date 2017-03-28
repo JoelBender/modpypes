3fab7ffca3b0a1174883f9d03bba0c023f75876f
Joel Bender <joel@betelgeuse>
2017-03-28 14:50:14 -0400

    mirror the BACpypes release script

--- release_to_pypi.sh
+++ release_to_pypi.sh
@@ -3,10 +3,26 @@
 # remove everything in the current dist/ directory
 [ -d dist ] && rm -Rfv dist
 
-for ver in 2.7 3.4; do
-    python$ver setup.py bdist_egg
-    python$ver setup.py bdist_wheel
+# start with a clean build directory
+rm -Rfv build/
+
+# Python 2.5 doesn't support wheels
+# python2.5 setup.py bdist_egg
+# rm -Rfv build/
+
+for version in 2.7 3.4 3.5 3.6; do
+    if [ -a "`which python$version`" ]; then
+        python$version setup.py bdist_egg
+        python$version setup.py bdist_wheel
+        rm -Rfv build/
+    fi
 done
+
+echo
+echo	This is what was built...
+echo
+ls -1 dist/
+echo
 
 read -p "Upload to PyPI? [y/n/x] " yesno || exit 1
 
